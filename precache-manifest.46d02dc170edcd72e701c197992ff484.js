e4204b58a605b752fb0a91b2984750354f46950b
chaetesh <chaetesh@gmail.com>
2023-02-14 09:13:27 +0530

    Updates

---
+++ precache-manifest.46d02dc170edcd72e701c197992ff484.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e6326d10da25b791026a34a74668f5",
+    "url": "/react_quiz_App/index.html"
+  },
+  {
+    "revision": "4d00afc28b6b3e78eb0e",
+    "url": "/react_quiz_App/static/css/2.fde0df24.chunk.css"
+  },
+  {
+    "revision": "6f2447191421a4d11520",
+    "url": "/react_quiz_App/static/css/main.38e1c9c6.chunk.css"
+  },
+  {
+    "revision": "4d00afc28b6b3e78eb0e",
+    "url": "/react_quiz_App/static/js/2.003cc1ef.chunk.js"
+  },
+  {
+    "revision": "c21d579849576bf6a47550572ff70f62",
+    "url": "/react_quiz_App/static/js/2.003cc1ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f2447191421a4d11520",
+    "url": "/react_quiz_App/static/js/main.b61d496c.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/react_quiz_App/static/js/main.b61d496c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "920b2ec5fd92a6d5fbbf",
+    "url": "/react_quiz_App/static/js/runtime-main.3d9849ba.js"
+  }
+]);
